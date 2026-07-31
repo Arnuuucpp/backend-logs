04a743e21854200b2de62d79d23b95acc93f0d66
Arnav <itsarnav3546@gmail.com>
2026-07-31 20:40:03 +0530

    Refactor server.js to establish DB connection and clean up app.j

--- lec90/server.js
+++ lec90/server.js
@@ -6,6 +6,17 @@
 
 const app = require("./src/app")
 
+const mongoose  = require("mongoose")
+
+function connectToDB() {
+    mongoose.connect("URI/lec90") //dont push code with URI (copy URI from compass)
+    .then(()=>{
+        console.log("Connected to DB");
+    })
+}
+
+connectToDB()
+
 app.listen(3000, (req,res)=>{
     console.log("server  is  running on port 3000");
 })
--- lec90/src/app.js
+++ lec90/src/app.js
@@ -1,15 +1,5 @@
 const express =  require("express");
-const mongoose  = require("mongoose")
 
-const app = express();
-
-function connectToDB() {
-    mongoose.connect("URI/lec90") //dont push code with URI (copy URI from compass)
-    .then(()=>{
-        console.log("Connected to DB");
-    })
-}
-
-connectToDB()
+const app = express()
 
 module.exports = app
