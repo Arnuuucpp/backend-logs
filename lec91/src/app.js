22683d16ffac3d3c8c9fa41d042beec684838dfe
Arnav <itsarnav3546@gmail.com>
2026-08-01 01:26:09 +0530

    added a new endpoint

--- lec91/src/app.js
+++ lec91/src/app.js
@@ -20,4 +20,20 @@ app.post("/notes",async (req,res)=>{
     })
 })
 
+/*
+GET /notes
+This endpoint allows clients to retrieve all notes from the database. The server will fetch the notes and return them in the response.
+
+for  this   we'll use the find method of mongoose model to get all the notes  from the  database in a ARRAY OF OBJECTS format
+*/
+
+app.get("/notes",  async (rej,res)=>{
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message: " notes found successfully",
+        notes: notes
+    })
+})
+
 module.exports = app
