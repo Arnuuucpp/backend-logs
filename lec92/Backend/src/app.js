ba74e2abc5403c6c98b2fcb178dea8e6a8fd5f27
Arnav <itsarnav3546@gmail.com>
2026-08-03 14:32:58 +0530

    add delete and update endpoints

--- lec92/Backend/src/app.js
+++ lec92/Backend/src/app.js
@@ -18,6 +18,16 @@ app.post("/api/notes", async (req,res)=>{
         message:"note created successfully",
         newNote
     })
+
+    //can  be done also  using .save()
+    /**
+    const Note = new Note({
+    title:"Node.js",
+    description:"Backend Basics"
+    });
+
+    await note.save();
+     */
 })
 
 /**
@@ -26,12 +36,43 @@ app.post("/api/notes", async (req,res)=>{
  */
 
 app.get("/api/notes", async (req,res)=>{
-    const notes = await noteModel.find()
+    const notes = await noteModel.find()    //find method returns data in  array of objects
     res.status(200).json({
         message:"fetched all notes",
         note:notes
     })
 })
+
+/**
+ * - DELETE /api/notes/:id
+ * - delete note by using the params
+ */
+
+app.delete("/api/notes/:id", async (req,res)=>{
+    const id = req.params.id
+
+    await noteModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message:"Note Deleted Successfully"
+    })
+})
+
+/**
+ * - PATCH /api/notes/:id
+ * - Update note by using the params
+ */
+
+app.patch("/api/notes/:id",async (req,res)=>{
+    const id = req.params.id
+    const {description} = req.body
+    await noteModel.findByIdAndUpdate(id,{description})  //to  note here is after id description is passed in object
+    res.status(200).json({
+        message:"note updated  successfully."
+    })
+})
+
+
 
 
 module.exports = app
