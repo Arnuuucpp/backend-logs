5aec33cbca2c0d9c406086a3255d70193b9cb714
Arnav <itsarnav3546@gmail.com>
2026-08-11 23:17:57 +0530

    refactor: streamline note fetching logic in App component

--- lec92/Frontend/src/App.jsx
+++ lec92/Frontend/src/App.jsx
@@ -5,16 +5,13 @@ import { useEffect } from "react";
 const App = () => {
   const [notes, setnotes] = useState([]);
 
-  function fetchNotes(){
-    axios.get("http://localhost:3000/api/notes")
-    .then((res) => {
-      setnotes(res.data.note);
-    });
-  }
-
-  
   useEffect(() => {
-    fetchNotes()
+    axios
+      .get("http://localhost:3000/api/notes")
+      .then((res) => {
+        setnotes(res.data.notes || []);
+      })
+      .catch((err) => console.error("Failed to fetch notes:", err));
   }, []);
 
   return (
@@ -22,7 +19,6 @@ const App = () => {
       <div className="bg-[#212121] h-screen p-8 flex gap-4">
         {notes.map((e, idx) => {
           return (
-            // <div className="bg-amber-300 ">
             <div
               className="bg-[#525151] p-4 mb-2 max-h-fit w-fit rounded-2xl  text-white "
               key={idx}
@@ -30,7 +26,6 @@ const App = () => {
               <h1 className="font-bold font-mono uppercase">{e.title}</h1>
               <p className="text-gray-300">{e.description}</p>
             </div>
-            // </div>
           );
         })}
       </div>
